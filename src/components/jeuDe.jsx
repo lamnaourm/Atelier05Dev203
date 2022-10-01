f4dc7cb013697d4ed4a069fde6e15955138f9c4b
lamnaourm <mohammed.lamnaour@gmail.com>
2022-10-01 13:09:26 +0100

    second commit

--- src/components/jeuDe.jsx
+++ src/components/jeuDe.jsx
@@ -10,20 +10,29 @@ class JeuDe extends Component {
   jouer() {
     const valeur = Math.floor(Math.random() * 6) + 1;
     let f = false;
-    if(this.props.cache===valeur)
-        f=true;
-    this.setState({face: valeur, compteur: this.state.compteur+1,fin:f});
+    if (this.props.cache === valeur) f = true;
+    this.setState({ face: valeur, compteur: this.state.compteur + 1, fin: f });
   }
   initialiser() {
     this.setState({ face: null, compteur: 0, fin: false });
   }
 
+  getImage() {
+    if(this.state.face==null)
+        return require('../images/facevide.png');
+    else 
+        return require('../images/face'+this.state.face+'.png');
+
+  }
+
   render() {
+    const styleImage = { width: "60px", height: "60px" };
     return (
       <div className="jeu">
         <img src={require("../images/jeuDe.png")} />
         <h1>Jeu de Dé</h1>
         <h2>face:{this.state.face}</h2>
+        <img src={this.getImage()} style={styleImage} />
         <h2>nombre d'essais {this.state.compteur} </h2>
         {!this.state.fin ? (
           <button onClick={() => this.jouer()}>jouer</button>
